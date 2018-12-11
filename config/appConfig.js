a4938d8452e9adc4f7a8d617a179166c0a63940e
SilentWolf <kezyolanipekun@gmail.com>
2018-12-11 01:10:19 +0100

    updated

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,5 +1,5 @@
 var API_BASE_URL = 'http://5bee92827839000013e6faed.mockapi.io/clane/api/v2/';
-var LIMIT = 10;
+var LIMIT = 12;
 var PAGE = 1;
 var CURRENT_NEWS = 'current_news';
 var networkErrorRetryTime = 3;
